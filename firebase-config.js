18cd7c29df758548b4d2e33176ceef8c69636560
adhamhasy <122464685+adhamhasy@users.noreply.github.com>
2026-08-10 20:49:15 +0300

    Remove Firebase Storage configuration
    Removed Firebase Storage imports and related exports.

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,3 @@
-// firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import {
   getDatabase,
@@ -8,12 +7,6 @@ import {
   onValue,
   remove,
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
-import {
-  getStorage,
-  ref as storageRef,
-  uploadBytes,
-  getDownloadURL
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB8nGEaOG7yRVZv_lBbWny3Zty03bbLwxE",
@@ -25,9 +18,7 @@ const firebaseConfig = {
   appId: "1:26278228643:web:62ea9854b1f250d7b84516"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
-const storage = getStorage(app);
 
-export { db, ref, push, set, onValue, remove, storage, storageRef, uploadBytes, getDownloadURL };
+export { db, ref, push, set, onValue, remove };
